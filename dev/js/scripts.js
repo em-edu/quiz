f22c93a05a9db94a9dce00b9a72f0e21e42f1b8a
em-edu <81432745+em-edu@users.noreply.github.com>
2022-04-14 21:05:03 -0400

    complete

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,7 +1,31 @@
-//import { gsap } from "gsap";
+import { gsap } from "gsap";
 
 //Replace this value with your name
-const yourName = "Your Name"
+const yourName = "Ethan"
 document.querySelector("#name").innerHTML = yourName;
 
 //GreenSock Timeline
+
+const mainTL = gsap.timeline();
+
+mainTL
+//part2a
+.from(".graph", {duration:2, alpha:0})
+//part1a
+.from(".red", {duration:1, x:0, ease:"bounce.out"},"p1")
+.from(".blue", {duration:1, x:350, ease:"bounce.out"},"p1")
+//part1b
+.to(".red", {duration:2, y:0, ease:"power3.out"},"p1a+=1")
+.to(".blue", {duration:2, y:300, ease:"power3.out"},"p1a+=1")
+//part3
+.to(".red", {duration:1, rotation:360, ease:"back.out"},"p3-=1")
+.to(".blue", {duration:1, rotation:360, ease:"back.out"},"p3-=1")
+//part2a
+.from(".green", {duration:mainTL.duration(), scaleX:0, transformOrigin:"0 0", ease:"none"},0)
+
+;
+
+
+//separate tween
+// gsap.from(".green", {duration:mainTL.duration(), scaleX:0, transformOrigin:"0 0", ease:"none"});
+
